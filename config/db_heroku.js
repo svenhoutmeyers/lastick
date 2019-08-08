70633b45455c068ba25aa949c699208bdb3110c9
Sven Houtmeyers <sven@cultuurnet.be>
2019-08-08 22:07:52 +0200

    removing switching db.js

--- config/db_heroku.js
+++ config/db_heroku.js
@@ -1,3 +1,6 @@
+process.env.DB_USER = 'svenhoutmeyers'
+process.env.DB_PASS = 'fBRkZAnR3JkwuY'
+
 module.exports = {
-  url : 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '.mlab.com:43127/heroku_0hffx6ds'
+  url : 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds343127.mlab.com:43127/heroku_0hffx6ds'
 };
